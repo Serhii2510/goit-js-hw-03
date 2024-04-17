670a028af81c09960ee3b583ef8d4db50ba65ecb
Serhii Ivashchenko <s.ivashchenko.1989@gmail.com>
2024-04-17 09:40:12 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,9 +2,9 @@
 
 function filterArray(numbers, value) {
     const newArray = [];
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) {
-            newArray.push(numbers[i]);
+    for (const element of numbers) {
+        if (element > value) {
+            newArray.push(element);
         }
     }
     return newArray;
